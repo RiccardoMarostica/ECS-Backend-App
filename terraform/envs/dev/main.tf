79f46c9a303671d786f895b03b15c0c314c3a118
Riccardo Marostica <46026232+RiccardoMarostica@users.noreply.github.com>
2025-10-17 17:18:07 +0200

    wip: added variables on main

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -38,4 +38,6 @@ module "rest_api" {
   rest_api_authorizer_name             = "${var.project_name}-${var.environment}-rest-api-authorizer"
   rest_api_authorizer_cognito_provider = [module.cognito_user_pool.cognito_user_pool_arn]
   rest_api_stage_name                  = var.environment
+  alb_listener_arn                     = module.alb.listener_arn
+  alb_vpc_link_id                      = module.alb.alb_vpc_link_id
 }
